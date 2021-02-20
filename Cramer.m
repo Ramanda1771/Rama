d91af7404e18ab21f54b6da7779213af22e09b41
Ramanda1771 <79345887+Ramanda1771@users.noreply.github.com>
2021-02-20 09:43:01 +0700

    Add files via upload

---
+++ Cramer.m
@@ -0,0 +1,34 @@
+clear;
+a = input ('matrik a = ');
+b = input ('matrik b = ');
+clc;
+disp ('matrik a = ');
+disp (a);
+disp ('matrik b = ');
+disp (b);
+Da = det (a);
+fprintf('Determinan a = %d \n \n', Da);
+[Ba, Ka] = size (a);
+[Bb, Kb] = size (b);
+if (Ba == Ka) & (Bb == Ba) & (Kb == 1)
+  if Da ~= 0
+    nb = a;
+    for j=1:Ba
+      for i=1:Ba
+        nb (i,j) = b (i,1);
+      end
+      Dnb = det (nb);
+      p = Dnb/Da;
+      fprintf('matrik bantu ke %d = \n',j);
+      fprintf('Determinan = %d \n',Dnb);
+      disp(nb);
+      fprintf('\n Penyelesaian ke %d = %.1f \n \n',j,p);
+      nb = a;
+    end
+  else
+    disp ('Tidak Ada Penyelesaian');
+  end
+else
+  disp ('matrik salah');
+  end
+  
